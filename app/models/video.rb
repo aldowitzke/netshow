b5334cb5330bd22845b27379725ad3b4de7b64d5
Aldo Witzke <aldo_mwa@hotmail.com>
2020-01-30 11:01:44 -0300

    add video format validation

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,8 @@
 class Video < ApplicationRecord
   validates_presence_of :name, :url
 
+  validates :url, format: { with: /(.m3u8)\z/,
+    message: 'extension must be .m3u8' }
+
   belongs_to :user
 end
